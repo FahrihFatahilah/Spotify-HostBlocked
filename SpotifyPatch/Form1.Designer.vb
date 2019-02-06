974ce0d5c72950c8e1858ff271e5ba4ad746b523
Agam <paichikusudo@gmail.com>
2019-02-06 03:27:38 -0800

    Spotify Host Blocker

---
+++ SpotifyPatch/Form1.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FormSkin1 = New WindowsApplication1.FormSkin()
+        Me.FlatTabControl1 = New WindowsApplication1.FlatTabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.FlatLabel1 = New WindowsApplication1.FlatLabel()
+        Me.FlatButton1 = New WindowsApplication1.FlatButton()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.FlatLabel3 = New WindowsApplication1.FlatLabel()
+        Me.FlatLabel2 = New WindowsApplication1.FlatLabel()
+        Me.FlatStatusBar1 = New WindowsApplication1.FlatStatusBar()
+        Me.FlatClose1 = New WindowsApplication1.FlatClose()
+        Me.FormSkin1.SuspendLayout()
+        Me.FlatTabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'FormSkin1
+        '
+        Me.FormSkin1.BackColor = System.Drawing.Color.White
+        Me.FormSkin1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.FormSkin1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.FormSkin1.Controls.Add(Me.FlatTabControl1)
+        Me.FormSkin1.Controls.Add(Me.FlatClose1)
+        Me.FormSkin1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FormSkin1.FlatColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FormSkin1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FormSkin1.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FormSkin1.HeaderMaximize = False
+        Me.FormSkin1.Location = New System.Drawing.Point(0, 0)
+        Me.FormSkin1.Name = "FormSkin1"
+        Me.FormSkin1.Size = New System.Drawing.Size(463, 201)
+        Me.FormSkin1.TabIndex = 0
+        Me.FormSkin1.Text = "FormSkin1"
+        '
+        'FlatTabControl1
+        '
+        Me.FlatTabControl1.ActiveColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatTabControl1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FlatTabControl1.Controls.Add(Me.TabPage1)
+        Me.FlatTabControl1.Controls.Add(Me.TabPage2)
+        Me.FlatTabControl1.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.FlatTabControl1.ItemSize = New System.Drawing.Size(120, 40)
+        Me.FlatTabControl1.Location = New System.Drawing.Point(3, 49)
+        Me.FlatTabControl1.Name = "FlatTabControl1"
+        Me.FlatTabControl1.SelectedIndex = 0
+        Me.FlatTabControl1.Size = New System.Drawing.Size(457, 152)
+        Me.FlatTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed
+        Me.FlatTabControl1.TabIndex = 2
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.TabPage1.Controls.Add(Me.FlatLabel1)
+        Me.TabPage1.Controls.Add(Me.FlatButton1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 44)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(449, 104)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Patch"
+        '
+        'FlatLabel1
+        '
+        Me.FlatLabel1.AutoSize = True
+        Me.FlatLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel1.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatLabel1.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel1.Location = New System.Drawing.Point(147, 16)
+        Me.FlatLabel1.Name = "FlatLabel1"
+        Me.FlatLabel1.Size = New System.Drawing.Size(134, 21)
+        Me.FlatLabel1.TabIndex = 1
+        Me.FlatLabel1.Text = "Click Patch Button"
+        '
+        'FlatButton1
+        '
+        Me.FlatButton1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FlatButton1.Location = New System.Drawing.Point(320, 50)
+        Me.FlatButton1.Name = "FlatButton1"
+        Me.FlatButton1.Rounded = False
+        Me.FlatButton1.Size = New System.Drawing.Size(106, 32)
+        Me.FlatButton1.TabIndex = 0
+        Me.FlatButton1.Text = "Patch"
+        Me.FlatButton1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'TabPage2
+        '
+        Me.TabPage2.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.TabPage2.Controls.Add(Me.FlatLabel3)
+        Me.TabPage2.Controls.Add(Me.FlatLabel2)
+        Me.TabPage2.Controls.Add(Me.FlatStatusBar1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 44)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(449, 104)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "About"
+        '
+        'FlatLabel3
+        '
+        Me.FlatLabel3.AutoSize = True
+        Me.FlatLabel3.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel3.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatLabel3.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel3.Location = New System.Drawing.Point(6, 39)
+        Me.FlatLabel3.Name = "FlatLabel3"
+        Me.FlatLabel3.Size = New System.Drawing.Size(51, 21)
+        Me.FlatLabel3.TabIndex = 2
+        Me.FlatLabel3.Text = "Agam"
+        '
+        'FlatLabel2
+        '
+        Me.FlatLabel2.AutoSize = True
+        Me.FlatLabel2.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel2.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatLabel2.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel2.Location = New System.Drawing.Point(6, 3)
+        Me.FlatLabel2.Name = "FlatLabel2"
+        Me.FlatLabel2.Size = New System.Drawing.Size(37, 21)
+        Me.FlatLabel2.TabIndex = 1
+        Me.FlatLabel2.Text = "Dev"
+        '
+        'FlatStatusBar1
+        '
+        Me.FlatStatusBar1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FlatStatusBar1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.FlatStatusBar1.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatStatusBar1.ForeColor = System.Drawing.Color.White
+        Me.FlatStatusBar1.Location = New System.Drawing.Point(3, 78)
+        Me.FlatStatusBar1.Name = "FlatStatusBar1"
+        Me.FlatStatusBar1.RectColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatStatusBar1.ShowTimeDate = False
+        Me.FlatStatusBar1.Size = New System.Drawing.Size(443, 23)
+        Me.FlatStatusBar1.TabIndex = 0
+        Me.FlatStatusBar1.Text = "Dev By Agam"
+        Me.FlatStatusBar1.TextColor = System.Drawing.Color.White
+        '
+        'FlatClose1
+        '
+        Me.FlatClose1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlatClose1.BackColor = System.Drawing.Color.White
+        Me.FlatClose1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(168, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.FlatClose1.Font = New System.Drawing.Font("Marlett", 10.0!)
+        Me.FlatClose1.Location = New System.Drawing.Point(442, 3)
+        Me.FlatClose1.Name = "FlatClose1"
+        Me.FlatClose1.Size = New System.Drawing.Size(18, 18)
+        Me.FlatClose1.TabIndex = 1
+        Me.FlatClose1.Text = "FlatClose1"
+        Me.FlatClose1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(463, 201)
+        Me.Controls.Add(Me.FormSkin1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.FormSkin1.ResumeLayout(False)
+        Me.FlatTabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents FormSkin1 As WindowsApplication1.FormSkin
+    Friend WithEvents FlatTabControl1 As WindowsApplication1.FlatTabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents FlatLabel1 As WindowsApplication1.FlatLabel
+    Friend WithEvents FlatButton1 As WindowsApplication1.FlatButton
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents FlatStatusBar1 As WindowsApplication1.FlatStatusBar
+    Friend WithEvents FlatClose1 As WindowsApplication1.FlatClose
+    Friend WithEvents FlatLabel3 As WindowsApplication1.FlatLabel
+    Friend WithEvents FlatLabel2 As WindowsApplication1.FlatLabel
+
+End Class
---
+++ SpotifyPatch/Form1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim path As String
+    Dim sw As StreamWriter
+    Private Sub FlatStatusBar1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FlatStatusBar1.Click
+
+    End Sub
+
+    Private Sub FlatLabel1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FlatLabel1.Click
+
+    End Sub
+
+    Private Sub FlatButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FlatButton1.Click
+        path = "C:\Windows\System32\drivers\etc\hosts"
+        sw = New StreamWriter(path, True)
+        Dim sitetoblock As String = (Environment.NewLine & "127.0.0.1 " & "adclick.g.doublecklick.net") 'has to be www.google.com | NOT: http://www.google.com/
+        Dim sitetoblock1 As String = (Environment.NewLine & "127.0.0.1 " & "adeventtracker.spotify.com") 'has to be www.google.com | NOT: http://www.google.com/
+        sw.Write(sitetoblock)
+        sw.Write(sitetoblock1)
+        sw.Close()
+        MessageBox.Show("Site Blocked")
+    End Sub
+End Class
